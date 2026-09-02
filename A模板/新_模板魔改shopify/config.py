01f77b367530d39437ecdb9673258b4f1f506d61
690878173 <690878173@qq.com>
2026-09-02 20:01:55 +0800

    新增模板

---
+++ A模板/新_模板魔改shopify/config.py
@@ -0,0 +1,54 @@
+# 请求头
+headers = None
+# cookie
+cookies = None
+# 重试次数
+max_retry = None
+# 超时时间
+time_out = None
+
+# NOTE =========
+
+base_url =
+site =
+zk = 0.3
+
+# NOTE =========
+
+# 图片链接自带逗号时，使用此分割
+images_split = None
+
+# 字典需要携带自定义字段时，使用字段
+custom_key = None
+
+# 'shopify'
+site_type = 'mg_shopify'
+
+
+from _ljp.base_tool import Base_tool,Tool_config
+
+
+config = Tool_config(
+    base_url=base_url,
+    site=site,
+    zk=zk,
+    site_type=site_type,
+    max_retry=max_retry,
+    time_out=time_out,
+    headers=headers,
+    cookies=cookies,
+    custom_key=custom_key,
+    images_split=images_split,
+    browser={'enabled': False},
+)
+
+Tool: Base_tool = Base_tool(config)
+
+
+
+
+
+
+
+
+
